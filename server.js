8b4546e6cb9df3528b7b4d65a0c34b3963a4d116
vanessa.padiernamazo <vanessa.padiernamazo@teleperformance.com>
2025-04-02 12:43:39 -0500

    subir server.js

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes'); 
 const userClientRoutes = require('./routes/userClientRoutes');
-const surveySet = require('./routes/surveyRoutes');
+const surveySetRoutes = require('./routes/surveyRoutes');
 const questionRoutes = require('./routes/questionRoutes');
 const endUserRoutes = require('./routes/endUserRoutes');
 const endUserClientRoutes = require('./routes/endUserClientRoutes');
+const clientsRoutes = require('./routes/clientRoute');
+const answersRoutes = require('./routes/answerRoute');
+
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -23,15 +27,17 @@ app.use(cors(corsOptions))
 app.use(express.json());
 app.use(cookieParser());
 
-
   
 // Rutas
 app.use('/auth', authRoutes);
-app.use('/user_clients', userClientRoutes); // Asigna las rutas de user_clients
-app.use('/surveys', surveySet); // Asigna las rutas de surveys
-app.use('/questions', questionRoutes); // Asigna las rutas de questions
-app.use('/endUsers', endUserRoutes); // Asigna las rutas de endUsers
-app.use('/endUserClients', endUserClientRoutes); // Asigna las rutas de endUserClients
+app.use('/api', userRoutes);
+app.use('/user_clients', userClientRoutes); 
+app.use('/surveys', surveySetRoutes);
+app.use('/questions', questionRoutes); 
+app.use('/endUsers', endUserRoutes); 
+app.use('/endUserClients', endUserClientRoutes); 
+app.use('/clients', clientsRoutes);
+app.use('/answer', answersRoutes);
 
 app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
