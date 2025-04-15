3001da044896941822dd9816109f9e4cdfb4969c
luis24rojas <lmmr.530@gmail.com>
2025-04-15 14:53:09 -0500

    Envio de correo masivo

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const endUserRoutes = require('./routes/endUserRoutes');
 const endUserClientRoutes = require('./routes/endUserClientRoutes');
 const clientsRoutes = require('./routes/clientRoute');
 const answersRoutes = require('./routes/answerRoutes');
+const enviarCorreos = require('./routes/mailRoutes');
 
 
 const app = express();
@@ -38,6 +39,7 @@ app.use('/api', endUserRoutes);
 app.use('/api', endUserClientRoutes); 
 app.use('/api', clientsRoutes);
 app.use('/api', answersRoutes);
+app.use('/api', enviarCorreos)
 
 app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
