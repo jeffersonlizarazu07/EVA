391071539ec4c83d37a6f30e81eb61c50df46ac7
Jefferson Lizarazu Rondon <lizarazurondon.5@teleperformance.co>
2025-04-15 12:22:31 -0500

    Cambios en rutas

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const corsOptions = {
-    origin: 'http://localhost:5173', 
+    origin: 'http://localhost:5174', 
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], 
     allowedHeaders: ['Content-Type', 'Authorization'], 
     credentials: true, 
