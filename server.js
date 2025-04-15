b3fdac145af9bfbe0f35a58817c0a16866a7cd7b
VanessaPadiernaMazo <vanepadierna@gmail.com>
2025-04-15 12:22:19 -0500

    Obtener main

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const corsOptions = {
-    origin: 'http://localhost:5173', 
+    origin: 'http://localhost:5174', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     allowedHeaders: ['Content-Type', 'Authorization'], 
     credentials: true, 
