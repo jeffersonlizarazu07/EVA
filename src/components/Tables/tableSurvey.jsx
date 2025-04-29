aadb2c30d6ea097944a5f2a5bf7442934e51b50e
VanessaPadiernaMazo <vanepadierna@gmail.com>
2025-04-29 15:59:06 -0500

    subir

--- src/components/Tables/tableSurvey.jsx
+++ src/components/Tables/tableSurvey.jsx
@@ -67,7 +67,7 @@ const TableSurvey = ({
 
   const filteredData = data.filter(
     (item) =>
-      userClients.some((client) => client.id == item.idClient) &&
+      //userClients.some((client) => client.id == item.idClient) &&
       Object.values(item).some(
         (val) =>
           typeof val == "string" &&
