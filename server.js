7563e3a01b90724d1af2e2396729d8a709b63c0f
Chezet07 <ChezetHernandez11@gmail.com>
2025-04-29 15:05:53 -0500

    Change
    Cambio para la conexión entre el backend y frontend

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const port = process.env.PORT || 3000;
 
 const corsOptions = {
 
-    origin: 'http://localhost:5174', 
+    origin: 'http://localhost:5173', 
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], 
     allowedHeaders: ['Content-Type', 'Authorization'], 
     credentials: true, 
