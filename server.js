410275fcb940d310694ae812f12c326e59582697
VanessaPadiernaMazo <vanepadierna@gmail.com>
2025-04-29 16:15:47 -0500

    subir

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const port = process.env.PORT || 3000;
 
 const corsOptions = {
 
-    origin: 'http://localhost:5173', 
+    origin: 'http://localhost:5174', 
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], 
     allowedHeaders: ['Content-Type', 'Authorization'], 
     credentials: true, 
