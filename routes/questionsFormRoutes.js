617d18c9727eebdcf3eb5eab536150fdbb655aaf
Luis Rojas <lmmr.530@gmail.com>
2025-05-27 15:53:47 -0500

    DTOs block. form y questionForm

--- routes/questionsFormRoutes.js
+++ routes/questionsFormRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const questionController = require('../controllers/questionsFormController');
 
 router.post('/', questionController.createQuestions);
-router.get('/block/:blockId', questionController.getQuestionsByBlockId);
+router.get('/block/:blockId?', questionController.getQuestionsByBlockId);
 router.put('/block/:blockId', questionController.updateQuestionsForBlock);
 
 module.exports = router;
