cc8f09301a52aab3b23fe78251bcb61205b1d410
Luis Rojas <lmmr.530@gmail.com>
2025-06-09 12:51:27 -0500

    manejo de error en el modelo

--- models/formSet.js
+++ models/formSet.js
@@ -3,44 +3,62 @@ const { getDateTimeForSQL } = require("../helpers/dateHelper");
 
 const FormSet = {
   getAll: () => {
-    return db("form_set")
-      .join("clients", "form_set.idClient", "=", "clients.id") // Relacionamos con la tabla de clientes
-      .join("users as creator", "form_set.created_by", "=", "creator.id") // Relacionamos con el creador
-      .leftJoin("users as updater", "form_set.updated_by", "=", "updater.id") // Relacionamos con el editor
-      .select(
-        "form_set.id", // Usamos 'id' en lugar de 'form_id'
-        "form_set.idClient", // Usamos 'idClient' en lugar de 'client_id'
-        "form_set.title", // Usamos 'title' en lugar de 'form_name'
-        "form_set.description", // Usamos 'description'
-        "form_set.creation_date", // Usamos 'creation_date'
-        "form_set.updated_date", // Usamos 'updated_date'
-        // Usamos la expresión CASE para transformar el valor de 'state' en texto
-        db.raw(
-          `CASE WHEN form_set.state = 1 THEN 'Activo' WHEN form_set.state = 0 THEN 'Inactivo' ELSE 'Desconocido' END as state`
-        ),
-        "clients.client as client_name", // Nombre del cliente
-        // Concatenamos el primer nombre y apellido del creador
-        db.raw(
-          'CONCAT(creator.firstname, " ", creator.lastname) as created_by_name'
-        ),
-        // Concatenamos el primer nombre y apellido del editor si existe
-        db.raw(
-          'IFNULL(DATE_FORMAT(updated_date, "%Y-%m-%d %H:%i:%s"), "No actualizada") as updated_date'
-        ),
-        db.raw(
-          'IFNULL(DATE_FORMAT(creation_date, "%Y-%m-%d %H:%i:%s"), "No actualizada") as creation_date'
-        ),
-        // Concatenamos el primer nombre y apellido del editor si existe
-        db.raw(
-          `IFNULL(CONCAT(updater.firstname, " ", updater.lastname), "No actualizada") as updated_by_name`
-        )
-      );
+    try {
+      return db("form_set")
+        .join("clients", "form_set.idClient", "=", "clients.id") // Relacionamos con la tabla de clientes
+        .join("users as creator", "form_set.created_by", "=", "creator.id") // Relacionamos con el creador
+        .leftJoin("users as updater", "form_set.updated_by", "=", "updater.id") // Relacionamos con el editor
+        .select(
+          "form_set.id", // Usamos 'id' en lugar de 'form_id'
+          "form_set.idClient", // Usamos 'idClient' en lugar de 'client_id'
+          "form_set.title", // Usamos 'title' en lugar de 'form_name'
+          "form_set.description", // Usamos 'description'
+          "form_set.creation_date", // Usamos 'creation_date'
+          "form_set.updated_date", // Usamos 'updated_date'
+          // Usamos la expresión CASE para transformar el valor de 'state' en texto
+          db.raw(
+            `CASE WHEN form_set.state = 1 THEN 'Activo' WHEN form_set.state = 0 THEN 'Inactivo' ELSE 'Desconocido' END as state`
+          ),
+          "clients.client as client_name", // Nombre del cliente
+          // Concatenamos el primer nombre y apellido del creador
+          db.raw(
+            'CONCAT(creator.firstname, " ", creator.lastname) as created_by_name'
+          ),
+          // Concatenamos el primer nombre y apellido del editor si existe
+          db.raw(
+            'IFNULL(DATE_FORMAT(updated_date, "%Y-%m-%d %H:%i:%s"), "No actualizada") as updated_date'
+          ),
+          db.raw(
+            'IFNULL(DATE_FORMAT(creation_date, "%Y-%m-%d %H:%i:%s"), "No actualizada") as creation_date'
+          ),
+          // Concatenamos el primer nombre y apellido del editor si existe
+          db.raw(
+            `IFNULL(CONCAT(updater.firstname, " ", updater.lastname), "No actualizada") as updated_by_name`
+          )
+        );
+    } catch (error) {
+      console.error('Error al crear la respuesta:', error);
+      throw new Error('No se pudo crear la respuesta debido a un error en el servidor.' + error.message);
+    }
   },
 
-  getById: (id) => db("form_set").where({ id }).first(),
+  getById: async (id) =>{
+    try {
+      const result = await db("form_set").where({ id }).first()
+      return result;
+    } catch (error) {
+      console.error('Error al crear la respuesta:', error);
+      throw new Error('No se pudo crear la respuesta debido a un error en el servidor.' + error.message);
+    }
+  },
 
   getByClients: (clientIdsArray) => {
-    return db("form_set").whereIn("idClient", clientIdsArray).select("*");
+    try {
+      return db("form_set").whereIn("idClient", clientIdsArray).select("*");
+    } catch (error) {
+      console.error("Error al obtener formularios por clientes:", error);
+      throw new Error("No se pudieron obtener los formularios debido a un error en el servidor." + error.message);
+    }
   },
 
   // create: (data) => db("form_set").insert(data),
@@ -82,17 +100,30 @@ const FormSet = {
     }
   },
 
-  toggleState: (id) => {
-    return db("form_set")
-      .where({ id })
-      .first()
-      .then((form) => {
-        if (!form) return null;
-        return db("form_set").where({ id }).update({ state: !form.state });
-      });
+  toggleState: async (id) => {
+    try {
+      return await db("form_set")
+        .where({ id })
+        .first()
+        .then((form) => {
+          if (!form) return null;
+          return db("form_set").where({ id }).update({ state: !form.state });
+        });
+    }catch (error) {
+      console.error("Error al cambiar el estado del formulario:", error);
+      throw new Error("No se pudo cambiar el estado del formulario debido a un error en el servidor." + error.message);
+    }
   },
 
-  delete: (id) => db("form_set").where({ id }).del(),
+  delete: async (id) => {
+    try {
+      const res = await db("form_set").where({ id }).del()
+      return res;
+    }catch (error) {
+      console.error("Error al eliminar el formulario:", error);
+      throw new Error("No se pudo eliminar el formulario debido a un error en el servidor." + error.message);
+    }
+  },
 };
 
 module.exports = FormSet;
