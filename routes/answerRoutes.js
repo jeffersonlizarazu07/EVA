bb9a6aa60803c72f79fc931da7bf53e8d8a3fe76
ghxstloner <yoinerdeyhan@gmail.com>
2025-08-14 10:49:07 -0500

    [add] top surveys

--- routes/answerRoutes.js
+++ routes/answerRoutes.js
@@ -20,7 +20,7 @@ router.get('/answers/:id?', authMiddleware, AnswerController.answersByQuestion);
 router.get('/answers/percentage/:id?',authMiddleware,  AnswerController.answersByQuestionPercentage);
 
 // Ruta para obtener el puntaje de las respuestas de una encuesta
-//router.get('/answers/survey/score', answerController.scoreXSurvey);
+router.get('/answers/survey/score', authMiddleware, AnswerController.scoreXSurvey);
 
 // Ruta para obtener los porcentajes de respuestas de una encuesta
 
